01127bcb14370bb99ddc9a40bef11954056453b7
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2016-01-08 14:58:29 +0200

    ** Added database model to save user uploaded files

---
+++ models/userFile.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var userFileSchema = new Schema({
+	data: Buffer,
+	contentType: String
+});
+
+module.exports = mongoose.model('UserFile', userFileSchema);
